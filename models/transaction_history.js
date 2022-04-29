f423dba7feb2a290996ed7fad05c9168905ec98c
rghanks <rghanks01@gmail.com>
2022-04-29 17:39:44 +0530

    corecte formatting

--- models/transaction_history.js
+++ models/transaction_history.js
@@ -3,12 +3,12 @@ const { isEmail } = require("validator");
 
 const transaction_history_Schema = new mongoose.Schema(
   {
-    email       : { type: String,required  : true,lowercase : true,validate  : [isEmail, "Please enter a valid email"]},   
-    symbol      : { type: String },
-    amount      : { type: Number, default: 0 },
-    status      : { type: Number, default: 0 },
-    to_address  : { type: String },
-    type        : { type: String }   
+    email: { type: String, required: true, lowercase: true, validate: [isEmail, "Please enter a valid email"] },
+    symbol: { type: String },
+    amount: { type: Number, default: 0 },
+    status: { type: Number, default: 0 },
+    to_address: { type: String },
+    type: { type: String }
   },
   { timestamps: true }
 );
